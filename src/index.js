ab88c2d2194f46dee320c24af9a7cc406b5d5c23
HannaKaliada <kaledaania2411@gmail.com>
2020-03-03 15:39:57 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map(brackets => brackets.join(''));
+    function replaceBrackets(string) {
+        for (let i = bracketsConfig.length - 1; i >= 0; i--) {
+            if (string.includes(bracketsConfig[i])) {
+                string = string.replace(bracketsConfig[i], '');
+                return replaceBrackets(string);
+            }
+            else if (string.length === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return replaceBrackets(str);
 }
